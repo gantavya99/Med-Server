d8f176b2a9df866174d6e8cacb04f07a2513a7d0
Gantavya Saraswat <gantavya99@gmail.com>
2023-05-07 00:09:56 +0530

    ip address added in mongo clusters

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
   });
 
- 
+ console.log()
 mongoose.connect('mongodb+srv://medpharmstore2023:medpharm123@cluster0.2orp8hr.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
