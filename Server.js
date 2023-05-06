8e4a65258df05e7a856a512074f1acd4909419b5
Gantavya Saraswat <gantavya99@gmail.com>
2023-05-06 22:15:03 +0530

    MongoDB connection done

---
+++ Server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+
+mongoose.connect('mongodb+srv://medpharmstore2023:medpharm123@cluster0.2orp8hr.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  
+})
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
+const port = 5000 || process.env.PORT;
+
+app.listen(port,()=>{
+    console.log(`Server listening on port ${port}`)
+})
