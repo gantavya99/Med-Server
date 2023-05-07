02a63696da84971b9f667d7fbfd582ce329b070c
Gantavya Saraswat <gantavya99@gmail.com>
2023-05-08 00:34:57 +0530

    Login Route Completed

--- Server.js
+++ Server.js
@@ -25,6 +25,7 @@ app.use(express.json());
 
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
+
 app.listen(port,()=>{
     console.log(`Server listening on port ${port}`)
 })
--- routes/auth.js
+++ routes/auth.js
@@ -6,33 +6,50 @@ const saltRounds = 10;
 
 //REGISTER
 router.post("/register", async (req, res) => {
-    const newUser = new User({
+    try {
+        // using await makes sure we store the hashed password in our DB and not the Promise
+      const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+      const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, saltRounds)
-    });
-    console.log("User Added Successfully");
-    //Save it to the DB
-    try {
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser)
+        password: hashedPassword,
+      });
+      console.log("User Added Successfully");
+      //Save it to the DB
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+    } catch (err) {
+      res.status(500).json(err);
     }
-    catch (err) {
-        res.status(500).json(err);
-    }
-});
-
+  });
+  
 
 //LOGIN
 
 router.post("/login", async (req, res) => {
     try {
-        const user = await User.findOne({ username: req.body.username });
-        const hash = bcrypt.compareSync(req.body.password,User.password);
+      const user = await User.findOne({ username: req.body.username });
+      if (!user) {
+        // user with the given username not found
+        return res.status(404).send();
+      }
+      // compare the entered password with the hashed password
+      const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+  
+      if (passwordMatch) {
+        // passwords match, user is authenticated
+        return res.status(200).json(user);
+        
+      } else {
+        // passwords don't match, user is not authenticated
+        return res.status(401).json("Wrong Password");
+      }
+  
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send();
     }
-    catch (err) {
-
-    }
-})
+  });
+  
 
 module.exports = router;
