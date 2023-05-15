e9894d8d08d77f31a0f3255908152964f1e39fb8
Gantavya Saraswat <gantavya99@gmail.com>
2023-05-15 21:49:18 +0530

    test4

--- routes/product.js
+++ routes/product.js
@@ -2,28 +2,30 @@ const router = require("express").Router();
 
 
 const Product = require('../models/Product');
+router.get("/",(req,res)=>{
+  res.send("API call successfull for products");
+})
 
 
+// router.get("/", async (req, res) => {
+//   const qNew = req.query.new;
+//   const qCategory = req.query.category;
+//   try {
+//     let products;
 
-router.get("/", async (req, res) => {
-  const qNew = req.query.new;
-  const qCategory = req.query.category;
-  try {
-    let products;
+//     if (qNew) {
+//       products = await Product.find().sort({ createdAt: -1 }).limit(5);
+//     } else if (qCategory) {
+//       products = await Product.find({category: qCategory });
+//     } else {
+//       products = await Product.find();
+//     }
 
-    if (qNew) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(5);
-    } else if (qCategory) {
-      products = await Product.find({category: qCategory });
-    } else {
-      products = await Product.find();
-    }
-
-    res.status(200).json(products);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.status(200).json(products);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 
 // Get a specific product by ID
