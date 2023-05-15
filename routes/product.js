abcd997bd9c2ff96afc98100db5a912f77665ed6
Gantavya Saraswat <gantavya99@gmail.com>
2023-05-15 21:56:44 +0530

    test5

--- routes/product.js
+++ routes/product.js
@@ -7,25 +7,25 @@ router.get("/",(req,res)=>{
 })
 
 
-// router.get("/", async (req, res) => {
-//   const qNew = req.query.new;
-//   const qCategory = req.query.category;
-//   try {
-//     let products;
+router.get("/all", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
 
-//     if (qNew) {
-//       products = await Product.find().sort({ createdAt: -1 }).limit(5);
-//     } else if (qCategory) {
-//       products = await Product.find({category: qCategory });
-//     } else {
-//       products = await Product.find();
-//     }
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (qCategory) {
+      products = await Product.find({category: qCategory });
+    } else {
+      products = await Product.find();
+    }
 
-//     res.status(200).json(products);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 // Get a specific product by ID
