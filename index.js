d8a7fbad4a25cf28e5a97ff5d1f500d67ab33556
Gantavya Saraswat <gantavya99@gmail.com>
2023-05-16 08:46:17 +0530

    test7

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const categoryRoute = require("./routes/category");
-const cors =require("cors");
+const cors = require("cors");
 dotenv.config();
-
-
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
@@ -20,22 +18,23 @@ mongoose.connect(process.env.MONGO_URL)
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
   });
-  
 
-const port = 8080 || process.env.PORT;
+const port = process.env.PORT || 8080;
+console.log(process.env.PORT);
 
- app.get("/",(req,res)=>{
-  res.send("Hello World!!")
- })
+app.get("/", (req, res) => {
+  res.send("Helldo World!!");
+});
+
 app.use(express.json());
 app.use(cors());
-app.use("/api/auth",authRoute);
-app.use("/api/users",userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 // app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-app.use("/api/products/category",categoryRoute);
+app.use("/api/products/category", categoryRoute);
 
-app.listen(port, "0.0.0.0" ,() => {
-    console.log(`Server listening on port ${port}`)
-})
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server listening on port ${port}`);
+});
