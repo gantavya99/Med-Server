0c147b77ab927b06d29c60053a31abba10e14609
Gantavya Saraswat <gantavya99@gmail.com>
2023-06-19 20:43:58 +0530

    test4

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/create-checkout-session', async (req, res) => {
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: '{{PRICE_ID}}',
+        price: '20',
         quantity: 1,
       },
     ],
