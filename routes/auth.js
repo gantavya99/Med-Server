ba36f0d0ac40acaf16b8c5d84affab3ef4f0b084
Gantavya Saraswat <gantavya99@gmail.com>
2023-07-20 02:23:28 +0530

    formatting

--- routes/auth.js
+++ routes/auth.js
@@ -9,25 +9,43 @@ dotenv.config();
 const secretKey = process.env.JWT_SEC;
 
 //REGISTER
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
     try {
-        // using await makes sure we store the hashed password in our DB and not the Promise
-        const newUser = new User({
-            email: req.body.email,
-            password: await bcrypt.hash(req.body.password, saltRounds),
-        });
-        console.log("User Added Successfully");
-        //Save it to the DB
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
+      const { email, password } = req.body;
+  
+      // Check if the email already exists in the database
+      const existingUser = await User.findOne({ email });
+  
+      if (existingUser) {
+        // If the email is already registered, return an error
+        return res.status(409).json({ error: 'Email is already registered' });
+      }
+  
+      // If the email is not registered, create a new user and save it to the database
+      const newUser = new User({
+        email,
+        password: await bcrypt.hash(password, saltRounds),
+      });
+  
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
     } catch (err) {
-        res.status(500).json(err);
+      res.status(500).json(err);
     }
-});
-
+  });
 
 //LOGIN
 
+router.get('/users', async (req, res) => {
+    try {
+      // Fetch all users from the database
+      const users = await User.find();
+      // Return the list of users as a JSON response
+      res.json(users);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to fetch users' });
+    }
+  });
 router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({ email: req.body.email });
